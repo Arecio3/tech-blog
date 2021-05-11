4ab28c8bae48fd19229b109a87c12b0b1f09e197
Arecio Canton <cuba289@gmail.com>
2021-05-10 22:49:32 -0400

    fixed some things

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { log } = require('handlebars');
 const { User, Comment, Blog } = require('../models');
 
 
@@ -15,6 +16,28 @@ router.get('/', async (req, res) => {
         console.log(err);
         res.status(500).json(err)
     }
+  });
+
+  router.get('/blogpost/:id', async (req, res) => {
+      try {
+          const postData = await Blog.findByPk(req.params.id);
+          console.log(postData);
+          const onePost = postData.get({ plain: true });
+          console.log({ onePost, loggedIn: req.session.loggedIn})
+          res.render('blogpost', { onePost, loggedIn: req.session.loggedIn})
+      } catch (err) {
+          console.log(err);
+          res.status(500).json(err)
+      }
+  })
+
+  
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
   });
 
   module.exports = router;
